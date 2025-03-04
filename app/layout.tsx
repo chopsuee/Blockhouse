1e5e0f5c8e2b0694da50f439b100d787bd4db67f
Kenneth <kennethhular15@gmail.com>
2025-03-04 10:26:28 +0800

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="p-5  w-full box-border"
+        className="p-5 w-full box-border"
       >
         {children}
       </body>
